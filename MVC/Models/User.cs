6af5a73bb700ed27d0dfb1ae2acf020c6378aeef
Dim <dimsxm@yandex.ru>
2023-09-20 10:12:13 +0300

    Добавьте файлы проекта.

---
+++ MVC/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC.Models
+{
+    /// <summary>
+    /// Описание пользователя в системе.
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsMarried { get; set; }
+    }
+}
