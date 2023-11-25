0c6e07f8f2e6adb48f108bd5521e7ad1b472629e
Dmitriy.malashenko <dmitriy.malashenko.ru>
2023-11-25 18:26:56 +0300

    контрорлер и вьюхи

---
+++ Cofee/Areas/Controllers/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cofee.Areas.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
