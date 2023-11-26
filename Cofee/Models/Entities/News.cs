1080218a181c1df3c21acdec8a5629d3a6ffbd4f
Dmitriy.malashenko <dmitriy.malashenko.ru>
2023-11-26 18:42:02 +0300

    DTO модели

---
+++ Cofee/Models/Entities/News.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cofee.Models.Entities
+{
+    /// <summary>
+    /// DTO Модель описывающая новость
+    /// </summary>
+    public class News
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string AuthorId { get; set; }
+        public User Author { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Cofee/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Cofee.Models
+{
+    /// <summary>
+    /// Модель DTO описыающая пользователя системы.
+    /// </summary>
+    public class User : IdentityUser
+    {
+        public DateTime Created { get; set; }
+    }
+}
